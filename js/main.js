1734f066d8e7be9d7ca8f4bb5aaec9776f1ac233
ShchegolevIvan <vangogishtv@gmail.com>
2024-04-24 00:41:55 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+
+    console.log("submit");
+
+    const tasktext = taskInput.value;
+    // console.log(tasktext);
+})
