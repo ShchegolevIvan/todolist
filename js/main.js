4375ff80d33b735c3a23bf9771866aaeab4153d6
ShchegolevIvan <vangogishtv@gmail.com>
2024-04-24 01:08:45 +0300

    Update main.js
    дописал form.addEventListener

--- js/main.js
+++ js/main.js
@@ -8,5 +8,18 @@ form.addEventListener('submit', function(e){
     console.log("submit");
 
     const tasktext = taskInput.value;
-   console.log(tasktext);
+   
+    const taskHTML =    `<li class="list-group-item d-flex justify-content-between task-item">
+                         <span class="task-title">${tasktext}</span>
+                            <div class="task-item__buttons">
+                                <button type="button" data-action="done" class="btn-action">
+                                     <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                                </button>
+                                    <button type="button" data-action="delete" class="btn-action">
+                                    <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                                </button>
+                            </div>
+                        </li>` ;
+
+    tasksList.insertAdjacentHTML('beforeend', taskHTML);
 })
